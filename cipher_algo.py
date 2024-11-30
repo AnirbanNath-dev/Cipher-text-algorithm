4024f6af27ab309250bff03b02af2f248494b4ec
Anirban Nath <anirbaniitian@gmail.com>
2024-11-30 19:28:39 +0530

    Updated logic

--- cipher_algo.py
+++ cipher_algo.py
@@ -1,27 +1,27 @@
 from time import sleep
 
-alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alphabets = 'abcdefghijklmnopqrstuvwxyz'
 
 def key():
     try:
         with open("key.txt","r") as f:
-            return f.readline().upper()
+            return f.readline().lower()
     except FileNotFoundError:
-        return "DEFAULT"
+        return "default"
     
 def letter_assign(word):
     
     alphabet_dict = {letter: i for i, letter in enumerate(alphabets , start=1)}
-    result = [alphabet_dict[letter.upper()] if letter.isalpha() else letter for letter in word]
+    result = [alphabet_dict[letter.lower()] if letter.isalpha() else letter for letter in word]
     
     return result
 
 def number_assign(list):
-    alphabet_dict = { i : letter for i , letter in enumerate(alphabets)}
+    alphabet_dict = { i : letter for i , letter in enumerate(alphabets, start=1)}
     result = []
     for i in range(len(list)):
         if type(list[i]) == int:
-            result.append(alphabet_dict[list[i]%26])
+            result.append(alphabet_dict[list[i]%26 if list[i] % 26 != 0 else 1])
         else:
             result.append(list[i])
     return result
@@ -43,10 +43,26 @@ def encrypt(text):
 
 
 def decrypt(text):
-    pass
+    key_list = letter_assign(key())
+    text_list = letter_assign(text)
+    
+    result = []
+    j = 0
+    for i in range(len(text_list)):
+        if type(text_list[i]) == int:
+            if text_list[i] < key_list[ j % len(key_list)]:
+                result.append(text_list[i] + 26 - key_list[j % len(key_list)])
+            else:
+                value = text_list[i] - key_list[j % len(key_list)]
+                result.append(value if value != 0 else 1 )
+            j+=1
+        else:
+            result.append(text_list[i])
+    
+    return "".join(number_assign(result))
 
 def main():
-    print("\n\t\tCHAT HERE ARE ENCRYPTED")
+    print("\n\t\tTEXTS HERE ARE ENCRYPTED")
     print("\t\t***********************\n\n")
     while True:
         option = input("Options:\n1.Encrypt\n2.Decrypt\n:")
@@ -54,12 +70,12 @@ def main():
             case "1":
                 normal_text = input("Type text to encrypt : ")
                 
-                encrypt(normal_text)
+                print(encrypt(normal_text))
 
             case "2":
                 encrypted_text = input("Type the encrypted text to decrypt : ")
                 
-                decrypt()
+                print(decrypt(encrypted_text))
 
             case _:
                 print("Invalid Option!")
